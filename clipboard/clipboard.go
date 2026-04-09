ec2af9d911a05ef9d4787e34fd5e92d340d409f0
Eric Dickens <eric@neovivify.com>
2026-04-09 16:04:10 -0400

    setting up csv parsing

--- clipboard/clipboard.go
+++ clipboard/clipboard.go
@@ -3,6 +3,7 @@ package clipboard
 
 import (
 	"os"
+        "io"
         "encoding/csv"
 
         "github.com/edickens09/sharedClipboard/commands"
@@ -36,20 +37,43 @@ func CopyToClipboard() error {
     defer file.Close()
     
     copiedText := commands.Paste()
-    if copiedText != 
+/*
+    if copiedText != content from csv {
+        then write to file
 
-    //copy from system clipboard to file make sure and increment the file
-
+        version += 1
+    }
+    copy from system clipboard to file make sure and increment the file
+*/
     return nil
     
 }
 
 func CopyFromClipboard( text string) error {
+    
+    var cbContent ClipboardContent
+
     file, err := os.Open("clipboard.dat")
     if err != nil {
         return err
     }
 
     defer file.Close()
+    
+    reader := csv.NewReader(file)
+
+    content, err := reader.Read()
+    if err != nil {
+        if err == io.EOF {
+           //does something need to go here. I'm just trying to catch this value and move past it. 
+        } else {
+            return err
+        }
+    }
+
+    cbContent.content = content[0]
+    //leaving this asis for now, I would like version to remain an int but csv needs to be string?
+    cbContent.version = content[1]
+    
     return nil
 }
